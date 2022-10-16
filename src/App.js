6a960b164028346408b1db7685315410ced20097
Caleb <caleb@calebcurry.com>
2022-10-16 09:21:57 -0400

    basic react query usage

--- src/App.js
+++ src/App.js
@@ -1,25 +1,38 @@
-import logo from './logo.svg';
 import './App.css';
+import { useQuery } from '@tanstack/react-query';
+import axios from 'axios';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    const priceQuery = useQuery(
+        ['price'],
+        () => {
+            return axios(
+                'https://api.coingecko.com/api/v3/simple/price?ids=bitcoin&vs_currencies=usd'
+            );
+        },
+        {
+            refetchInterval: 1000 * 5,
+        }
+    );
+
+    const customerQuery = useQuery(
+        ['customers'],
+        () => {
+            return axios('http://localhost:3000/api/customers');
+        },
+        {
+            refetchOnWindowFocus: true,
+        }
+    );
+    return (
+        <div className="App">
+            {priceQuery?.error ? <p>UH oh! Error...</p> : null}
+            {priceQuery?.isLoading ? <p>Loading...</p> : null}
+            {priceQuery?.data?.data?.bitcoin?.usd
+                ? priceQuery.data.data.bitcoin.usd
+                : null}
+        </div>
+    );
 }
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -2,16 +2,29 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
+import {
+    useQuery,
+    useMutation,
+    useQueryClient,
+    QueryClient,
+    QueryClientProvider,
+} from '@tanstack/react-query';
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
+
+const queryClient = new QueryClient({
+    defaultOptions: {
+        queries: {
+            refetchOnWindowFocus: false,
+        },
+    },
+});
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+    <React.StrictMode>
+        <QueryClientProvider client={queryClient}>
+            <App />
+            <ReactQueryDevtools initialIsOpen={true} />
+        </QueryClientProvider>
+    </React.StrictMode>
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
